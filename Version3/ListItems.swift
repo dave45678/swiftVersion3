0549c28b0b0ba88e2af76d6c23f21576eccb26b4
dave <dave45678@gmail.com>
2018-08-11 12:53:52 -0400

    initial version

---
+++ Version3/ListItems.swift
@@ -0,0 +1,19 @@
+//
+//  ListItems.swift
+//  Version3
+//
+//  Created by MC Student on 8/11/18.
+//  Copyright © 2018 MCStudent. All rights reserved.
+//
+
+import Foundation
+
+class ListItems{
+    var category:String
+    var description:String
+    
+    init(category:String,description:String){
+        self.category = category
+        self.description = description
+    }
+}
--- Version3/ViewController.swift
+++ Version3/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Celllistitems")!
+        let text = data[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
+    }
+    
+    //TODO:CREATE AN ARRAY OF LIST ITEMS
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for i in 0...20{
+            data.append("item \(i)")
+        }
+        tableView.dataSource = self
     }
+    private var data:[String] = []
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 }
 
